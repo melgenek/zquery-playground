511ab0529b51e7d5c184441f9bc1733e517d5d56
Yevhenii Melnyk <ymelnyk@liveintent.com>
2020-06-10 16:53:13 +0200

    add environment

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
-import zio.query.{DataSource, Request, UQuery, ZQuery}
-import zio.{Chunk, Runtime, ZIO}
+import zio.query.{DataSource, RQuery, Request, UQuery, ZQuery}
+import zio.{Chunk, Has, Runtime, ZIO}
+
+case class Bearer(value: String)
 
 case class User(id: Long, name: String, addressId: Long, paymentId: Long)
 case class Address(id: Long, street: String)
@@ -7,25 +9,29 @@ case class Payment(id: Long, name: String)
 
 object Sources {
 
-  case class GetPayment(id: Long) extends Request[Nothing, Payment]
-  val paymentSource = DataSource.fromFunctionBatchedOptionM("PaymentSource") { requests: Chunk[GetPayment] =>
-    ZIO.succeed(List(Payment(222, "sample222")))
-      .map { payments =>
-        requests.map(req => payments.find(_.id == req.id))
-      }
-  }
+  type HasToken = Has[Bearer]
 
-  def getPayment(id: Long): UQuery[Payment] = {
+  case class GetPayment(id: Long) extends Request[Nothing, Payment]
+  val paymentSource: DataSource[HasToken, GetPayment] =
+    DataSource.fromFunctionBatchedOptionM("PaymentSource") { requests: Chunk[GetPayment] =>
+      ZIO.succeed(List(Payment(222, "sample222")))
+        .map { payments =>
+          requests.map(req => payments.find(_.id == req.id))
+        }
+    }
+
+  def getPayment(id: Long): RQuery[HasToken, Payment] = {
     ZQuery.fromRequest(GetPayment(id))(paymentSource)
   }
 
   case class GetAddress(id: Long) extends Request[Nothing, Address]
-  val addressSource = DataSource.fromFunctionBatchedOptionM("AddressSource") { requests: Chunk[GetAddress] =>
-    ZIO.succeed(List(Address(111, "sample111")))
-      .map { addresses => requests.map(req => addresses.find(_.id == req.id)) }
-  }
+  val addressSource: DataSource[HasToken, GetAddress] =
+    DataSource.fromFunctionBatchedOptionM("AddressSource") { requests: Chunk[GetAddress] =>
+      ZIO.succeed(List(Address(111, "sample111")))
+        .map { addresses => requests.map(req => addresses.find(_.id == req.id)) }
+    }
 
-  def getAddress(id: Long): UQuery[Address] = {
+  def getAddress(id: Long): RQuery[HasToken, Address] = {
     ZQuery.fromRequest(GetAddress(id))(addressSource)
   }
 
@@ -33,6 +39,7 @@ object Sources {
 
 object Main extends App {
 
+  val bearerToken = Bearer("any")
   val result = for {
     users <- ZQuery.fromEffect(ZIO.succeed(List(User(1, "user1", 111, 222))))
     richUsers <- ZQuery.foreachPar(users) { user =>
@@ -44,6 +51,6 @@ object Main extends App {
     }
   } yield richUsers
 
-  println(Runtime.default.unsafeRun(result.run))
+  println(Runtime.default.unsafeRun(result.run.provide(Has(bearerToken))))
 
 }
